d7ea9e59b76a189217d94459cad6a518b49a43ce
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-24 11:17:12 +0400

    fix: 02 added comments to the pagination app

--- 02pagination.js
+++ 02pagination.js
@@ -1,9 +1,9 @@
 "use strict";
 const pagination = {
-  currentPage: 0,
+  currentPage: 0, //starts at 0 by default
   prevPage: function () {
     if (this.currentPage === 0) {
-      this.currentPage = this.itemsLength;
+      this.currentPage = this.itemsLength; //if we are on the first page(currentPage = 0) got to last page
       return pagination;
     } else {
       this.currentPage--;
@@ -12,7 +12,7 @@ const pagination = {
   },
   nextPage: function () {
     if ((this.currentPage = this.itemsLength)) {
-      this.currentPage = 0;
+      this.currentPage = 0; // if on the last page set currentPage to 0
       return pagination;
     } else {
       this.currentPage++;
@@ -25,12 +25,12 @@ const pagination = {
   init: function (items, perPage) {
     this.items = items;
     this.perPage = perPage;
-    this.itemsLength = Math.floor(items.length / perPage);
+    this.itemsLength = Math.floor(items.length / perPage); // creates a prop for page count
     const separatePages = [];
     for (let i = 0; i < this.items.length; i = i + this.perPage) {
       separatePages.push(this.items.slice(i, i + this.perPage));
     }
-    this.separatePages = separatePages;
+    this.separatePages = separatePages; // creates an array with every single page items for us to call
   },
   getPageItems: function () {
     return this.separatePages[this.currentPage];
@@ -42,6 +42,7 @@ const pagination = {
 
 const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
 pagination.init(alphabetArr, 4);
+console.log(pagination.itemsLength);
 console.log(pagination.currentPage);
 pagination.nextPage().nextPage();
 console.log(pagination.currentPage);
